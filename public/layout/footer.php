fa88427cb6c17e5353332546549522846b69e532
isakbet <yosephbet@gmail.com>
2022-06-24 11:18:38 +0200

    Add footer.php and header.php

---
+++ public/layout/footer.php
@@ -0,0 +1,15 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-lg-8 mx-auto">
+                <ul class="list-inline text-center">
+                    <li class="list-inline-item"><a href="https://twitter.com/yosephbet" target="_blank"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-twitter fa-stack-1x fa-inverse"></i></span></a></li>
+                    <li class="list-inline-item"><a href="https://www.linkedin/in/yosephbet/" target="_blank"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-linkedin fa-stack-1x fa-inverse"></i></span></a></li>
+                    <li class="list-inline-item"><a href="https://github.com/isakbet" target="_blank"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-github fa-stack-1x fa-inverse"></i></span></a></li>
+                </ul>
+                <p class="text-muted copyright">&copy; 2019 - <?php echo date('Y'); ?> <a href="https://yoseph.dev">Yoseph Berhane</a>. All rights
+                    reserved</p>
+            </div>
+        </div>
+    </div>
+</footer>
