9aa2b8b3d32f3cd2962c3898c1b7ab2fb1fe55fb
isakbet <yosephbet@gmail.com>
2022-06-24 14:51:56 +0200

    Import sb-admin-2.css and min.css for admin

---
+++ public/admin/layout/footer.php
@@ -0,0 +1,8 @@
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span class="text-muted">&copy; 2019 - <?php echo date('Y'); ?> <a href="#">Jhey's Blog</a>. All rights
+                reserved</span>
+        </div>
+    </div>
+</footer>
