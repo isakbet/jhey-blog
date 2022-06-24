e1471efc2dc00962c4a7473d7fe9007306ab1fb1
isakbet <yosephbet@gmail.com>
2022-06-24 14:55:09 +0200

    Import header.php and sidebar.php

---
+++ public/admin/layout/sidebar.php
@@ -0,0 +1,47 @@
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
+        <div class="sidebar-brand-icon rotate-n-15">
+            <i class="fas fa-laugh-wink"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">Admin Panel<sup>2</sup></div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item active">
+        <a class="nav-link" href="index.php">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Heading -->
+    <div class="sidebar-heading">
+        Interface
+    </div>
+
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
+            <i class="fas fa-fw fa-folder"></i>
+            <span>Pages</span>
+        </a>
+        <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Users:</h6>
+                <a class="collapse-item" href="add-users.php">Add Users</a>
+                <a class="collapse-item" href="manage-users.php">Manage Users</a>
+                <div class="collapse-divider"></div>
+                <h6 class="collapse-header">Posts:</h6>
+                <a class="collapse-item" href="add-post.php">Add Posts</a>
+                <a class="collapse-item" href="manage-posts.php">Manage Posts</a>
+            </div>
+        </div>
+    </li>
+</ul>
