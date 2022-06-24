b62fb7547919773708b4d5ae0403ad7bf58b66e9
isakbet <yosephbet@gmail.com>
2022-06-24 14:23:45 +0200

    Change dbh to pdo

--- public/layout/header.php
+++ public/layout/header.php
@@ -27,7 +27,7 @@
                 <?php } else {
                     $email = $_SESSION['login'];
                     $sql = "SELECT `fname`,`lname` FROM `users` WHERE `email`=:email";
-                    $query = $dbh->prepare($sql);
+                    $query = $pdo->prepare($sql);
                     $query->bindParam(':email', $email, PDO::PARAM_STR);
                     $query->execute();
                     $results = $query->fetchAll(PDO::FETCH_OBJ);
