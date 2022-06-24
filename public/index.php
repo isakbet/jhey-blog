38c093e960ea0d63593bdacc5eef422f97c5d115
isakbet <yosephbet@gmail.com>
2022-06-24 11:29:21 +0200

    Add header with class, div class and h1 elements

---
+++ public/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <!-- Google Fonts -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic">
+  <!-- Styling for publika sidan -->
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+  <!-- <link rel="stylesheet" type="text/css" href="styles.css"> -->
+  <title>Jhey's-blog | Home</title>
+</head>
+
+<body>
+  <header class="masthead" style="background-image:url('assets/img/home-bg.jpg');">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-10 col-lg-8 mx-auto">
+          <div class="site-heading">
+            <h1>Jhey's Blog</h1><span class="subheading">Personal Blog</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+  <main>
+  </main>
+  <footer>
+  </footer>
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
