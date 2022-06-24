cc31c14e70085bd59fd3c4a84e476d04194f1273
isakbet <yosephbet@gmail.com>
2022-06-24 10:55:53 +0200

    Import fonts, fonte-awesome, bootstrap css and js

--- src/index.php
+++ src/index.php
@@ -1,15 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="">
   <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Averia+Serif+Libre|Noto+Serif|Tangerine" rel="stylesheet">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic">
   <!-- Styling for publika sidan -->
-  <link rel="stylesheet" type="text/css" href="styles.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+  <!-- <link rel="stylesheet" type="text/css" href="styles.css"> -->
   <title>Jhey's-blog | Home</title>
 </head>
+
 <body>
   <header>
   </header>
@@ -17,6 +22,8 @@
   </main>
   <footer>
   </footer>
-  <script src="scripts.js"></script>
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>  
 </body>
+
 </html>
